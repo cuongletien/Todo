879b01636c01df13d89815e65d388053d555ca07
Le Cuong <cuonglt1408@gmail.com>
2020-04-11 21:22:36 +0700

    test jest finAll todo

--- models/tests/todo.spec.js
+++ models/tests/todo.spec.js
@@ -28,12 +28,31 @@ describe('Test todo model', () => {
     expect(todos.length).toBe(0);
   });
 
-  it('should delete todo with id', () => {
+  it('Should return false if could not found todo id', () => {
     insert({ id: 1, title: 'First Todo' });
     const errDelete = deleteById(0);
     expect.assertions(2);
     expect(errDelete).toBeFalsy();
     expect(todos.length).toBe(1);
   });
-  
+
+  it('Should return all todos', () => {
+    insert({ id: 2, title: 'Second Todo'});
+    let listTodo = findAll();
+    expect(listTodo).toEqual(todos);
+  });
+
+  it('Should return all todo with completed true', () => {
+    updateById({ id: 2, completed: true});
+    let listTodo = findAll({completed: true});
+    expect(listTodo.length).toBe(1);
+  });
+
+  it('Should return all todo with completed false', () => {
+    insert({ id: 3, title: 'Third Todo' });
+    insert({ id: 4, title: '4th Todo' });
+    let listTodo = findAll({ completed: false });
+    expect(listTodo.length).toBe(3);
+  });
+
 });
